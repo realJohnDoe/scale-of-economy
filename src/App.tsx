d85515e2f6944ed98c00022340057c8559280124
Johannes Döllinger <johannes.doellinger@gmail.com>
2026-01-04 17:01:20 +0100

    Try flex-1

--- src/App.tsx
+++ src/App.tsx
@@ -163,7 +163,7 @@ function App() {
       </div>
 
       <div
-        className="relative w-full grow min-h-0"
+        className="relative w-full flex-1 min-h-0"
         onTouchStart={handleTouchStart}
         onTouchEnd={handleTouchEnd}
       >
